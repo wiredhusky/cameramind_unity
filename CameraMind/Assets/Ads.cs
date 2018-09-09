618307587720399269900d9977f23fb0d923cf1a
wiredhusky <wired@tinacinc.com>
2018-09-10 08:39:14 +0900

    ads
    1. Ads 연동 토대만 닦아 놓음
    2. 내 상태 저장하는 기능 구현해야 함
    3. Hint 구현
    4. 그래야 Ads 보고 보상받는 거 테스트 가능함

---
+++ CameraMind/Assets/Ads.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+
+    private const string rewarded_video_id = "rewardedVideo";
+
+	public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady(rewarded_video_id))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show(rewarded_video_id, options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Finished");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Skipped");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Failed");
+                break;                
+        }
+    }
+
+}
