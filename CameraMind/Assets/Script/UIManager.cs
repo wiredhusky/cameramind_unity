22fadf6d8cbce818f01e0d9f5f6c31069c2e6c19
wiredhusky <wired@tinacinc.com>
2018-09-12 08:36:45 +0900

    Nothing
    1. 버튼 버그 잡느라 시간 다 씀
    2. UIManager랑 Timer 결합해야 하지 않을까 고민...

--- CameraMind/Assets/Script/UIManager.cs
+++ CameraMind/Assets/Script/UIManager.cs
@@ -9,6 +9,7 @@ public class UIManager : MonoBehaviour {
     Animator animator;
     public int hintCount = 3;
     public TextMeshProUGUI hintText;
+    public bool isHintPressed = false;
 
     private void Start()
     {
@@ -16,16 +17,31 @@ public class UIManager : MonoBehaviour {
     }
 
     public void HintPressed()
-    {        
+    {
+        isHintPressed = true;
         switch (SpawnPrefab.instance.scene)
         {
             case "Track":
+                animator = SpawnPrefab.instance.obj[SpawnPrefab.instance.index_track].GetComponent<Animator>();
+                animator.SetTrigger("Hint");
+                break;
+            case "Alone":
+                animator = SpawnPrefab.instance.obj[SpawnPrefab.instance.index_alone].GetComponent<Animator>();
+                animator.SetTrigger("Hint");
+                break;
+            case "Time Attack":
+                Timer.timerControl.setTimer = false;
+                Timer.timerControl.animator.speed = 0;
+                animator = SpawnPrefab.instance.obj[SpawnPrefab.instance.index].GetComponent<Animator>();
+                animator.SetTrigger("Hint");
                 break;
             default:
                 animator = SpawnPrefab.instance.obj[SpawnPrefab.instance.index].GetComponent<Animator>();
                 animator.SetTrigger("Hint");
                 break;
         }
+        hintCount--;
+        hintText.text = "x " + hintCount.ToString();
     }
 
     public void PausePressed()
